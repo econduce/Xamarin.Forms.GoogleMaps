e52ce5ed5fb2723bb6a6f1aeac502a9a0b49b4c3
amay077 <okuokuoku@gmail.com>
2017-12-14 01:44:07 +0900

    code for repro

--- XFGoogleMapSample/XFGoogleMapSample/BasicMapPage.xaml.cs
+++ XFGoogleMapSample/XFGoogleMapSample/BasicMapPage.xaml.cs
@@ -11,159 +11,34 @@ namespace XFGoogleMapSample
     {
         public BasicMapPage()
         {
-            InitializeComponent();
+            //InitializeComponent();
 
-            // MapTypes
-            var mapTypeValues = new List<MapType>();
-            foreach (var mapType in Enum.GetValues(typeof(MapType)))
+            var camera = CameraUpdateFactory.NewPositionZoom(new Position(35.71d, 139.815d), 10d);
+            map = new Map
             {
-                mapTypeValues.Add((MapType)mapType);
-                pickerMapType.Items.Add(Enum.GetName(typeof(MapType), mapType));
-            }
-
-            pickerMapType.SelectedIndexChanged += (sender, e) =>
-            {
-                map.MapType = mapTypeValues[pickerMapType.SelectedIndex];
-            };
-            pickerMapType.SelectedIndex = 0;
-
-            // MyLocationEnabled
-            switchMyLocationEnabled.Toggled += (sender, e) =>
-            {
-                map.MyLocationEnabled = e.Value;
-            };
-            switchMyLocationEnabled.IsToggled = map.MyLocationEnabled;
-
-            // IsTrafficEnabled
-            switchIsTrafficEnabled.Toggled += (sender, e) =>
-            {
-                map.IsTrafficEnabled = e.Value;
-            };
-            switchIsTrafficEnabled.IsToggled = map.IsTrafficEnabled;
-
-            // IndoorEnabled
-            switchIndoorEnabled.Toggled += (sender, e) =>
-            {
-                map.IsIndoorEnabled = e.Value;
-            };
-            switchIndoorEnabled.IsToggled = map.IsIndoorEnabled;
-
-            // CompassEnabled
-            switchCompassEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.CompassEnabled = e.Value;
-            };
-            switchCompassEnabled.IsToggled = map.UiSettings.CompassEnabled;
-
-            // RotateGesturesEnabled
-            switchRotateGesturesEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.RotateGesturesEnabled = e.Value;
-            };
-            switchRotateGesturesEnabled.IsToggled = map.UiSettings.RotateGesturesEnabled;
-
-            // MyLocationButtonEnabled
-            switchMyLocationButtonEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.MyLocationButtonEnabled = e.Value;
-            };
-            switchMyLocationButtonEnabled.IsToggled = map.UiSettings.MyLocationButtonEnabled;
-
-            // IndoorLevelPickerEnabled
-            switchIndoorLevelPickerEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.IndoorLevelPickerEnabled = e.Value;
-            };
-            switchIndoorLevelPickerEnabled.IsToggled = map.UiSettings.IndoorLevelPickerEnabled;
-
-            // ScrollGesturesEnabled
-            switchScrollGesturesEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.ScrollGesturesEnabled = e.Value;
-            };
-            switchScrollGesturesEnabled.IsToggled = map.UiSettings.ScrollGesturesEnabled;
-
-            // TiltGesturesEnabled
-            switchTiltGesturesEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.TiltGesturesEnabled = e.Value;
-            };
-            switchTiltGesturesEnabled.IsToggled = map.UiSettings.TiltGesturesEnabled;
-
-            // ZoomControlsEnabled
-            switchZoomControlsEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.ZoomControlsEnabled = e.Value;
-            };
-            switchZoomControlsEnabled.IsToggled = map.UiSettings.ZoomControlsEnabled;
-
-            // ZoomGesturesEnabled
-            switchZoomGesturesEnabled.Toggled += (sender, e) =>
-            {
-                map.UiSettings.ZoomGesturesEnabled = e.Value;
-            };
-            switchZoomGesturesEnabled.IsToggled = map.UiSettings.ZoomGesturesEnabled;
-
-            // Map Clicked
-            map.MapClicked += (sender, e) =>
-            {
-                var lat = e.Point.Latitude.ToString("0.000");
-                var lng = e.Point.Longitude.ToString("0.000");
-                this.DisplayAlert("MapClicked", $"{lat}/{lng}", "CLOSE");
+                VerticalOptions = LayoutOptions.Fill,
+                HorizontalOptions = LayoutOptions.Fill,
+                IsIndoorEnabled = false,
+                IsTrafficEnabled = false,
+                //HasRotationEnabled = false,
+                //HasScrollEnabled = false,
+                //HasZoomEnabled = false,
+                //IsShowingUser = false,
+                MapType = MapType.Street,
+                InitialCameraUpdate = camera,
             };
 
-            // Map Long clicked
-            map.MapLongClicked += (sender, e) =>
-            {
-                var lat = e.Point.Latitude.ToString("0.000");
-                var lng = e.Point.Longitude.ToString("0.000");
-                this.DisplayAlert("MapLongClicked", $"{lat}/{lng}", "CLOSE");
-            };
+            map.UiSettings.CompassEnabled = false;
+            map.UiSettings.IndoorLevelPickerEnabled = false;
+            map.UiSettings.MyLocationButtonEnabled = false;
+            map.UiSettings.RotateGesturesEnabled = false;
+            map.MyLocationEnabled = false;
+            map.UiSettings.ScrollGesturesEnabled = false;
+            map.UiSettings.TiltGesturesEnabled = false;
+            map.UiSettings.ZoomControlsEnabled = false;
+            map.UiSettings.ZoomGesturesEnabled = false;
 
-            // Map MyLocationButton clicked
-            map.MyLocationButtonClicked += (sender, args) =>
-            {
-                args.Handled = switchHandleMyLocationButton.IsToggled;
-                if (switchHandleMyLocationButton.IsToggled)
-                {
-                    this.DisplayAlert("MyLocationButtonClicked", 
-                                 "If set MyLocationButtonClickedEventArgs.Handled = true then skip obtain current location", 
-                                 "OK");
-                }
-
-            };
-
-            map.CameraChanged += (sender, args) =>
-            {
-                var p = args.Position;
-                labelStatus.Text = $"Lat={p.Target.Latitude:0.00}, Long={p.Target.Longitude:0.00}, Zoom={p.Zoom:0.00}, Bearing={p.Bearing:0.00}, Tilt={p.Tilt:0.00}";
-            };
-
-            // Geocode
-            buttonGeocode.Clicked += async (sender, e) =>
-            {
-                var geocoder = new Xamarin.Forms.GoogleMaps.Geocoder();
-                var positions = await geocoder.GetPositionsForAddressAsync(entryAddress.Text);
-                if (positions.Count() > 0)
-                {
-                    var pos = positions.First();
-                    map.MoveToRegion(MapSpan.FromCenterAndRadius(pos, Distance.FromMeters(5000)));
-                    var reg = map.VisibleRegion;
-                    var format = "0.00";
-                    labelStatus.Text = $"Center = {reg.Center.Latitude.ToString(format)}, {reg.Center.Longitude.ToString(format)}";
-                }
-                else
-                {
-                    await this.DisplayAlert("Not found", "Geocoder returns no results", "Close");
-                }
-            };
-
-            // Snapshot
-            buttonTakeSnapshot.Clicked += async (sender, e) =>
-            {
-                var stream = await map.TakeSnapshot();
-                imageSnapshot.Source = ImageSource.FromStream(() => stream);
-            };
+            Content = map;
         }
     }
 }
