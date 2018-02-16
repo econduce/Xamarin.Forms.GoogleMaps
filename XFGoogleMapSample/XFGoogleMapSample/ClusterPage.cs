2975caef0597ca20eca961be34d37ce052f2cad8
Oscar de los Rios <oscar@econduce.mx>
2018-02-16 11:32:32 -0600

    Added clustered pins support from issue 123

---
+++ XFGoogleMapSample/XFGoogleMapSample/ClusterPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace XFGoogleMapSample
+{
+    public class ClusterPage : ContentPage
+    {
+        public ClusterPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
